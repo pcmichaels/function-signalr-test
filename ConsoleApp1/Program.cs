b8ffdecde10bf9ace000def883d2b05200f03c8d
Paul Michaels <pcmichaels@live.com>
2020-01-22 08:26:47 +0000

    Initial check in of test code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Press any key to send a message");
+            Console.ReadLine();
+
+            HttpClient client = new HttpClient();
+            string url = "http://localhost:7071/api/messages";
+            
+            HttpContent content = new StringContent("{'Value': 'Hello'}", Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            string results = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine($"results: {results}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/getmessages.js
@@ -0,0 +1,28 @@
+﻿function bindConnectionMessage(connection) {
+    var messageCallback = function (name, message) {
+        if (!message) return;
+        // deal with the message
+        console.log("message received:" + message);
+    };
+    // Create a function that the hub can call to broadcast messages.
+    connection.on('broadcastMessage', messageCallback);
+    connection.on('echo', messageCallback);
+    connection.on('receive', messageCallback);
+}
+
+function onConnected(connection) {
+    console.log("onConnected called");
+}
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl('/infoRelay')
+    .build();
+
+bindConnectionMessage(connection);
+connection.start()
+    .then(function () {
+        onConnected(connection);
+    })
+    .catch(function (error) {
+        console.error(error.message);
+    });
